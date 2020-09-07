a27b9e19ef57a0918ca76624aa7083f0e4e15d75
007lva <lvargas@rankia.com>
2020-09-07 15:50:20 +0200

    Fix Safari webp-support
    is=webp-support should be added when loading is not present or equal to
    'eager'

--- handlers/image-handler.js
+++ handlers/image-handler.js
@@ -5,14 +5,12 @@ class ImageHandler {
   }
 
   element (element) {
-    if (element.hasAttribute('loading')) {
-      if (element.getAttribute('loading') === 'lazy') {
+    if (element.getAttribute('loading') === 'lazy') {
         element.setAttribute('data-src', element.getAttribute('src'))
         element.setAttribute('is', this.customElement)
         element.removeAttribute('loading')
         element.removeAttribute('src')
-      }
-    } else if (!element.hasAttribute('is')) {
+    } else if ((!element.hasAttribute('loading') || element.getAttribute('loading') === 'eager') && !element.hasAttribute('is')) {
       if (element.getAttribute('src').endsWith('.webp') && !this.supportsWebP) {
         element.setAttribute('is', 'webp-support')
       }
