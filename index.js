50c4b8590e0a8afd7546df63c1a6456369fd6dee
007lva <lvargas@rankia.com>
2020-10-29 16:42:18 +0100

    Fix WebP support on iOS < 14

--- index.js
+++ index.js
@@ -8,37 +8,49 @@ async function handleRequest(request) {
   const browser = Bowser.getParser(userAgent)
   const rewriter = new HTMLRewriter()
 
-  const supportsLazyLoadIframe = browser.satisfies({
-    chrome: '>85',
-    firefox: '>84'
-  })
+  const os = browser.parseOS()
+  const osName = os.name.toLowerCase()
+  const osVersion = os.version
 
-  if (!supportsLazyLoadIframe) {
+  // iOS < 14 is problematic and need special treatment
+  if (osName === 'ios' && !isNaN(osVersion) && parseFloat(osVersion) < 14) {
     const iframeHandler = new IframeHandler()
+    const imageHandler = new ImageHandler(false)
     rewriter.on('iframe[loading=lazy]', iframeHandler)
-
-    const supportsLazyLoadImage = browser.satisfies({
-      chrome: '>84',
-      firefox: '>75',
-      edge: '>80',
-      mobile: {
-        chrome: '>84'
-      }
+    rewriter.on('img', imageHandler)
+  } else {
+    const supportsLazyLoadIframe = browser.satisfies({
+      chrome: '>85',
+      firefox: '>84'
     })
 
-    if (!supportsLazyLoadImage) {
-      const supportsWebP = browser.satisfies({
-        chrome: '>32',
-        firefox: '>65',
-        safari: '>15',
-        edge: '>18',
+    if (!supportsLazyLoadIframe) {
+      const iframeHandler = new IframeHandler()
+      rewriter.on('iframe[loading=lazy]', iframeHandler)
+
+      const supportsLazyLoadImage = browser.satisfies({
+        chrome: '>84',
+        firefox: '>75',
+        edge: '>80',
         mobile: {
-          firefox: '>68'
+          chrome: '>84'
         }
       })
 
-      const imageHandler = new ImageHandler(supportsWebP)
-      rewriter.on('img', imageHandler)
+      if (!supportsLazyLoadImage) {
+        const supportsWebP = browser.satisfies({
+          chrome: '>32',
+          firefox: '>65',
+          safari: '>15',
+          edge: '>18',
+          mobile: {
+            firefox: '>68'
+          }
+        })
+
+        const imageHandler = new ImageHandler(supportsWebP)
+        rewriter.on('img', imageHandler)
+      }
     }
   }
 
